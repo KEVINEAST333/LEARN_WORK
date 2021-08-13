ad32a8af626a71f51a00d8d8b7ea620efe947a8d
kevinwang <dong_w333@163.com>
2021-08-13 11:21:54 +0800

    push again

--- 2021_8_13_learn/src/TestGit.java
+++ 2021_8_13_learn/src/TestGit.java
@@ -1,5 +1,9 @@
 public class TestGit {
     public static void main(String[] args) {
+
         System.out.println("hello git learn work");
+        //测试增加内容
+        System.out.println("git push again");
+
     }
 }
